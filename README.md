104fcf31067ddd213b3da52bb59c75705f6bccf0
Phellipe Silva <phellipealexandre@gmail.com>
2016-07-11 17:07:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Android Converter Dojo
+Código base para aplicação Android que será utilizada no Dojo de conversão númerica
---
+++ app/src/main/java/com/converterdojo/pafsilva/androidconverterdojo/ConverterActivity.java
@@ -0,0 +1,13 @@
+package com.converterdojo.pafsilva.androidconverterdojo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
